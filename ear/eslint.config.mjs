3d9f0faeb90116878584892e821f75c45185e1dd
notsugat <sugatsujakhu@gmail.com>
2024-12-13 18:10:50 +0545

    refactor: change to typescript

--- ear/eslint.config.mjs
+++ ear/eslint.config.mjs
@@ -1,9 +1,26 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+	{
+		files: ["**/*.{js,mjs,cjs,ts}"],
+		languageOptions: {
+			parser: tsParser,
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+		},
+		plugins: {
+			"@typescript-eslint": tseslint,
+		},
+		rules: {
+			...pluginJs.configs.recommended.rules,
+			...tseslint.configs.recommended.rules,
+			"no-unused-vars": "off",
+			"@typescript-eslint/no-unused-vars": ["warn"],
+		},
+	},
 ];
